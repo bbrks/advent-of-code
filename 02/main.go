c5aa3d4e52868cb0b16ac9a196a41007a8a89b79
Ben Brooks <ben@bbrks.me>
2015-12-02 10:09:13 +0000

    Initial commit of Day 2 Part 1

---
+++ 02/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+// Returns the surface area of a rectangular cuboid
+// plus the area of the smallest side as extra
+func surfaceArea(b []int) int {
+	surfaceArea := 2*b[0]*b[1] + 2*b[0]*b[2] + 2*b[1]*b[2]
+	extra := b[0] * b[1] // smallest side
+	return surfaceArea + extra
+}
+
+func main() {
+	sqFt := 0
+
+	file, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var b []int
+		ds := strings.Split(scanner.Text(), "x")
+		for _, d := range ds {
+			i, err := strconv.Atoi(d)
+			if err != nil {
+				panic(err)
+			}
+			b = append(b, i)
+		}
+		sort.Ints(b) // so we can get the smallest side by [0]*[1]
+		sqFt = sqFt + surfaceArea(b)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Total sqft of wrapping paper needed:", sqFt)
+}
