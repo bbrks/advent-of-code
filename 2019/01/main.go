ebb9984e03787ec10adaf5b5eccab13332e857f9
Ben Brooks <ben@bbrks.me>
2019-12-01 10:34:11 +0000

    Add 2019/01 Part 2

--- 2019/01/main.go
+++ 2019/01/main.go
@@ -8,15 +8,26 @@ import (
 	"strconv"
 )
 
-// FuelRequirement calculates the required fuel for a single module mass.
-func FuelRequirement(moduleMass int) (fuelRequired int) {
+// FuelRequirement1 calculates the required fuel for a single module mass for part 1.
+func FuelRequirement1(moduleMass int) (fuelRequired int) {
 	return moduleMass/3 - 2
 }
 
+// FuelRequirement2 calculates the required fuel for a single module mass for part 2.
+func FuelRequirement2(mass int) (fuelRequired int) {
+	fuelRequired = FuelRequirement1(mass)
+	// clamp to zero
+	if fuelRequired < 0 {
+		return 0
+	}
+	fuelRequired += FuelRequirement2(fuelRequired)
+	return fuelRequired
+}
+
 // TotalFuelRequirements calculates the sum of the required fuel for the given module masses.
-func TotalFuelRequirements(moduleMasses ...int) (totalFuelRequired int) {
+func TotalFuelRequirements(calcFn func(int) int, moduleMasses ...int) (totalFuelRequired int) {
 	for _, moduleMass := range moduleMasses {
-		totalFuelRequired += FuelRequirement(moduleMass)
+		totalFuelRequired += calcFn(moduleMass)
 	}
 	return totalFuelRequired
 }
@@ -42,6 +53,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Total fuel required: %v", TotalFuelRequirements(moduleMasses...))
+	fmt.Printf("Total fuel required part 1: %v\n", TotalFuelRequirements(FuelRequirement1, moduleMasses...))
+	fmt.Printf("Total fuel required part 2: %v\n", TotalFuelRequirements(FuelRequirement2, moduleMasses...))
 
 }
--- 2019/01/main_test.go
+++ 2019/01/main_test.go
@@ -7,21 +7,46 @@ import (
 
 func TestFuelRequirement(t *testing.T) {
 	tests := []struct {
-		input          int
-		expectedOutput int
+		input           int
+		expectedOutput1 int
+		expectedOutput2 int
 	}{
-		{12, 2},
-		{14, 2},
-		{1969, 654},
-		{100756, 33583},
+		{12, 2, 2},
+		{14, 2, 2},
+		{1969, 654, 966},
+		{100756, 33583, 50346},
 	}
+
+	const (
+		totalOutput1 = 34241
+		totalOutput2 = 51316
+	)
 
 	for _, test := range tests {
 		t.Run(strconv.Itoa(test.input), func(t *testing.T) {
-			output := FuelRequirement(test.input)
-			if output != test.expectedOutput {
-				t.Errorf("output %d did not match expected %d", output, test.expectedOutput)
+			output := FuelRequirement1(test.input)
+			if output != test.expectedOutput1 {
+				t.Errorf("output 1 %d did not match expected %d", output, test.expectedOutput1)
+			}
+			output = FuelRequirement2(test.input)
+			if output != test.expectedOutput2 {
+				t.Errorf("output 2 %d did not match expected %d", output, test.expectedOutput2)
 			}
 		})
 	}
+
+	t.Run("TotalFuelRequirements", func(t *testing.T) {
+		var input []int
+		for _, test := range tests {
+			input = append(input, test.input)
+		}
+		output := TotalFuelRequirements(FuelRequirement1, input...)
+		if output != totalOutput1 {
+			t.Errorf("total output 1 %d did not match expected %d", output, totalOutput1)
+		}
+		output = TotalFuelRequirements(FuelRequirement2, input...)
+		if output != totalOutput2 {
+			t.Errorf("total output 2 %d did not match expected %d", output, totalOutput2)
+		}
+	})
 }
