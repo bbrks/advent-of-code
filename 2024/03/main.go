58a7187512b7198793ce8965fbff597082342532
Ben Brooks <ben.brooks@couchbase.com>
2024-12-03 12:09:11 +0000

    2024-12-03

---
+++ 2024/03/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	b, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	val, err := dayTwoPartOne(string(b))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("day one part one: %d\n", val)
+
+	val, err = dayTwoPartTwo(string(b))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("day one part two: %d\n", val)
+}
+
+func dayTwoPartOne(input string) (int, error) {
+	instructions, err := parseInput(input)
+	if err != nil {
+		return 0, err
+	}
+	val := 0
+	for _, ins := range instructions {
+		switch ins.op {
+		case "mul":
+			val += ins.a * ins.b
+		}
+	}
+	return val, nil
+}
+
+func dayTwoPartTwo(input string) (int, error) {
+	instructions, err := parseInput(input)
+	if err != nil {
+		return 0, err
+	}
+	val := 0
+	do := true
+	for _, ins := range instructions {
+		switch ins.op {
+		case "do":
+			do = true
+		case "don't":
+			do = false
+		case "mul":
+			if do {
+				val += ins.a * ins.b
+			}
+		}
+	}
+	return val, nil
+}
+
+type instruction struct {
+	a, b int
+	op   string
+}
+
+func parseInput(input string) ([]instruction, error) {
+	reports := make([]instruction, 0)
+
+	mulRegexp := regexp.MustCompile(`(mul|do|don't)\((\d*),?(\d*)\)`)
+	matches := mulRegexp.FindAllStringSubmatch(input, -1)
+	for _, match := range matches {
+		if len(match) != 4 {
+			return nil, fmt.Errorf("invalid input %q", match)
+		}
+		var a, b int
+		if match[2] != "" {
+			ai64, err := strconv.ParseInt(match[2], 10, 64)
+			if err != nil {
+				return nil, fmt.Errorf("invalid input %q: %w", match[2], err)
+			}
+			a = int(ai64)
+		}
+		if match[3] != "" {
+			bi64, err := strconv.ParseInt(match[3], 10, 64)
+			if err != nil {
+				return nil, fmt.Errorf("invalid input %q: %w", match[3], err)
+			}
+			b = int(bi64)
+		}
+		reports = append(reports, instruction{a: a, b: b, op: match[1]})
+	}
+
+	return reports, nil
+}
---
+++ 2024/03/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+func TestDayThree(t *testing.T) {
+	tests := []struct {
+		input           string
+		expectedPartOne int
+		expectedPartTwo int
+	}{
+		{
+			input:           `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`,
+			expectedPartOne: 161,
+			expectedPartTwo: 161,
+		},
+		{
+			input:           `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`,
+			expectedPartOne: 161,
+			expectedPartTwo: 48,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run("part 1", func(t *testing.T) {
+			result, err := dayTwoPartOne(test.input)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if result != test.expectedPartOne {
+				t.Errorf("expected %d but got %d", test.expectedPartOne, result)
+			}
+		})
+		t.Run("part 2", func(t *testing.T) {
+			result, err := dayTwoPartTwo(test.input)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if result != test.expectedPartTwo {
+				t.Errorf("expected %d but got %d", test.expectedPartTwo, result)
+			}
+		})
+	}
+}
